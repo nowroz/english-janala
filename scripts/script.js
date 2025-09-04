944a4712f14afa17c2c8ed9095d8f24ce0ba0e52
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2025-09-04 08:32:27 +0600

    WIP: Add let's learn section
    Load the lesson buttons dynamically

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,29 @@
 console.log("'script.js' is connected");
+
+const url = "https://openapi.programming-hero.com/api";
+
+const loadLessons = () => {
+  const endpoint = url + "/levels/all";
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((result) => displayLesson(result.data));
+};
+
+const displayLesson = (lessons) => {
+  const lvlButtonContainer = document.getElementById("lvl-btn-container");
+  lvlButtonContainer.innerHTML = "";
+
+  for (const lesson of lessons) {
+    const newButton = document.createElement("button");
+    newButton.className =
+      "font-poppins text-sm font-semibold text-[#422AD5] border border-[#422AD5] rounded px-3 py-2 hover:text-white hover:bg-[#422AD5] cursor-pointer";
+    newButton.innerHTML = `
+            <span><i class="fa-solid fa-book-open"></i></span>
+            Lesson-${lesson.level_no}
+`;
+
+    lvlButtonContainer.appendChild(newButton);
+  }
+};
+
+loadLessons();
